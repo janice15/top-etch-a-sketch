291a6cddac5a20e643e5b04a5a153028d30fcfde
Janice <41716156+janice15@users.noreply.github.com>
2026-08-31 20:03:46 -0700

    add formula to calculate width and height of grid square

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function drawGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("div-square");
+    let gridSize = 100 / size + "%";
+    square.style.width = gridSize;
+    square.style.height = gridSize;
     gridContainer.append(square);
   }
 }
 
-drawGrid(16);
+drawGrid(64);
